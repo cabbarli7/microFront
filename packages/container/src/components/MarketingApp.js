356eb21322f31041a692d74d89e9bbf20b28ee16
Elvin <cabbarli.elvin75@gmail.com>
2025-10-03 23:49:31 +0300

    First commit

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useRef } from "react";
+import { mount } from "marketing/Marketing";
+
+export const MarketingApp = () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+  return <div ref={ref} />;
+};
---
+++ packages/marketing/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
+import { BrowserRouter, Route, Switch } from "react-router-dom/";
+import { StylesProvider } from "@material-ui/core";
+
+const App = () => {
+  return (
+    <StylesProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/pricing" component={Pricing} />
+          <Route path="/" component={Landing} />
+        </Switch>
+      </BrowserRouter>
+    </StylesProvider>
+  );
+};
+
+export default App;
